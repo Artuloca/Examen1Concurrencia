ff3e0bcf181a37393024ed745c9fc0fcf0853d57
Artuloca <116103814+Artuloca@users.noreply.github.com>
2024-10-12 11:34:47 +0200

    Add files via upload

---
+++ demo (1)/demo/src/main/java/com/example/examenconcurrencia/ExamenConcurrenciaApplication.java
@@ -0,0 +1,13 @@
+package com.example.examenconcurrencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenConcurrenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenConcurrenciaApplication.class, args);
+	}
+
+}
---
+++ demo (1)/demo/src/test/java/com/example/examenconcurrencia/ExamenConcurrenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examenconcurrencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenConcurrenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
